4f4c079b4eb1e397a7e3502941643529ae82a0e6
italosantana <flaubert165@gmail.com>
2017-12-07 16:56:03 -0300

    architecture fixes

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -6,8 +6,10 @@ import play.api.libs.json.Json
 import play.api.mvc._
 import services.ChatService
 
+import scala.concurrent.ExecutionContext
+
 @Singleton
-class ChatController @Inject()(chatService: ChatService) extends Controller {
+class ChatController @Inject()(chatService: ChatService)(implicit exec: ExecutionContext) extends Controller {
 
   def send = Action.async {
     this.chatService.send().map { msg => Ok(Json.toJson(msg)) }
