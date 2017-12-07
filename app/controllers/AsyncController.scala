0e1c848fdd1468190a1894a28ae28b4499f8bb61
italosantana <flaubert165@gmail.com>
2017-12-07 17:56:49 -0300

    hot fix

--- app/controllers/AsyncController.scala
+++ app/controllers/AsyncController.scala
@@ -25,7 +25,7 @@ class AsyncController @Inject()(cc: ControllerComponents, actorSystem: ActorSyst
 
     getFutureMessage(1.second).map { msg => Ok(msg) }
   }
-  
+
   private def getFutureMessage(delayTime: FiniteDuration): Future[String] = {
     val promise: Promise[String] = Promise[String]()
     actorSystem.scheduler.scheduleOnce(delayTime) {
